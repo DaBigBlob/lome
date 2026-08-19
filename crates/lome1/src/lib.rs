e1501e2757b5dc6c292c15a96b9b76c75bf72b21
Person <localboxcrox@gmail.com>
2026-08-19 16:24:55 -0400

    auto-generated commit message; unmemorable commit [[not in multi-developer repo]]

--- crates/lome1/src/lib.rs
+++ crates/lome1/src/lib.rs
@@ -11,16 +11,45 @@
 
 extern crate alloc;
 use alloc::boxed::Box;
-use lome0 as modus;
+use lome0;
 
-type App<M> = modus::Tree<M>;
+// leaf is now constructor
+// pub trait ConApplicator<Con> {
+//     /// Owned handle for one application.
+//     type ApplicatorTask;
 
-// in PTS outside, types, kinda, ... (axioms) will be constructors
+//     /// Begin or schedule `operator operand`.
+//     fn task(&mut self, operator:Con, operand:Con) -> Self::ApplicatorTask;
 
-/// Expression: Abstraction + Application
+//     /// Wait for or execute `task`, then return its application result.
+//     fn completed(&mut self, task:Self::ApplicatorTask) -> Tree<Con>;
+// }
+
+/*
+
+we only need to care about reducing to App(Con, Con).
+we will now decide and fill in when the
+
+(Con, Con) is taken from above
+app is handled below.
+we need to construct pure App in terms of Con.
+
+*/
+
+// type AppTree<L> = lome0::Tree<L>;
+
+// we must recognize that we are the Leaf implementors
 #[derive(PartialEq, Eq)]
-pub enum Tree<Leaf>{
-    Abs(Box<(Tree<Leaf>, Tree<Leaf>)>),
-    App(Box<App<Tree<Leaf>>>),
-    Lea(Leaf),
+pub enum AppLeaf<Con>{
+    Abs(Box<(AppLeaf<Con>, AppLeaf<Con>)>),
+    App(AppTree<Con>),
+    Lea(Con)
 }
+// now we impl apply on all pairs of Tree<Con>
+// (Con, Con) -> Tree<Con> is offloaded to above
+
+pub type AppTree<Con> = lome0::Tree<Box<AppLeaf<Con>>>;
+
+// we must now implement applicator for (Leaf, Leaf)
+// - that is our responsibility, of which,
+// (Leaf::Con, Leaf::Con) is the responsibility of above level
