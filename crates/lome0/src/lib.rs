34c01148c09c065f0474b3609fbd2a2912e52ba9
Person <localboxcrox@gmail.com>
2026-08-19 21:26:13 -0400

    auto-generated commit message; unmemorable commit [[not in multi-developer repo]]

--- crates/lome0/src/lib.rs
+++ crates/lome0/src/lib.rs
@@ -54,22 +54,6 @@ pub trait LeafApplicator<Leaf> {
     fn completed(&mut self, task:Self::ApplicatorTask) -> Tree<Leaf>;
 }
 
-/// Applicator impl for (ToTask, FromTask) function tuple
-impl<
-    Leaf,
-    Task,
-    ToTask:Fn(Leaf, Leaf) -> Task,
-    FromTask:Fn(Task) -> Tree<Leaf>
-> LeafApplicator<Leaf> for (ToTask, FromTask) {
-    type ApplicatorTask = Task;
-
-    fn task(&mut self, operator: Leaf, operand: Leaf) -> Self::ApplicatorTask
-    {self.0(operator, operand)}
-
-    fn completed(&mut self, task: Self::ApplicatorTask) -> Tree<Leaf>
-    {self.1(task)}
-}
-
 /// Principal Expression
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Tree<Leaf> {
--- crates/lome1/src/lib.rs
+++ crates/lome1/src/lib.rs
@@ -26,27 +26,51 @@ pub trait ConApplicator<Con> {
 // we must recognize that we are the Leaf implementors
 pub enum Leaf<Con>{
     Abs(Box<(Leaf<Con>, Leaf<Con>)>),
-    App(Box<Tree<Con>>),
+    App(Box<OTree<Con>>),
     Con(Con)
 }
 
-pub struct Tree<Con>(lome0::Tree<Leaf<Con>>);
-impl <Con> From<lome0::Tree<Leaf<Con>>> for Tree<Con> {
+pub struct OTree<Con>(lome0::Tree<Leaf<Con>>);
+impl <Con> From<lome0::Tree<Leaf<Con>>> for OTree<Con> {
     fn from(value: lome0::Tree<Leaf<Con>>) -> Self {Self(value)}
 }
-impl <Con> Into<lome0::Tree<Leaf<Con>>> for Tree<Con> {
+impl <Con> Into<lome0::Tree<Leaf<Con>>> for OTree<Con> {
     fn into(self) -> lome0::Tree<Leaf<Con>> {self.0}
 }
-impl <Con> Tree<Con> {}
+impl <Con> OTree<Con> {
+    pub fn norm<A: ConApplicator<Con>>
+    (self, applicator:&mut A) -> Leaf<Con> {
+        match self.0 {
+            lome0::Tree::Lea(lf) => lf,
+            lome0::Tree::Brc(_) => todo!(),
+        }
+    }
+}
+
+/// Applicator impl for (ToTask, FromTask) function tuple
+// impl<
+//     Leaf,
+//     Task,
+//     ToTask:FnMut(Leaf, Leaf) -> Task,
+//     FromTask:FnMut(Task) -> Tree<Leaf>
+// > LeafApplicator<Leaf> for (ToTask, FromTask) {
+//     type ApplicatorTask = Task;
+
+//     fn task(&mut self, operator: Leaf, operand: Leaf) -> Self::ApplicatorTask
+//     {self.0(operator, operand)}
+
+//     fn completed(&mut self, task: Self::ApplicatorTask) -> Tree<Leaf>
+//     {self.1(task)}
+// }
 
 mod application {
 use alloc::boxed::Box;
 
-use crate::{ConApplicator, Leaf, Tree};
+use crate::{ConApplicator, Leaf, OTree};
 
 pub(super) enum Task<Con, A: ConApplicator<Con>> {
     ConTask(A::ConTask),
-    Tree(Tree<Con>)
+    Tree(OTree<Con>)
 }
 
 pub(super) fn task<Con, A: ConApplicator<Con>>
@@ -61,7 +85,7 @@ pub(super) fn task<Con, A: ConApplicator<Con>>
 }
 
 pub(super) fn completed<Con, A: ConApplicator<Con>>
-(task:Task<Con, A>, ator:&mut A) -> Tree<Con> {
+(task:Task<Con, A>, ator:&mut A) -> OTree<Con> {
     match task {
         Task::ConTask(ct)
         => lome0::Tree::Lea(Leaf::Con(ator.completed(ct))).into(),
